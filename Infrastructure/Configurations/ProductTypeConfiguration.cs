ab46ad99e41d15dad7280de981a457febf8f26ab
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-09-18 10:04:50 +0200

    Add entities configuration

---
+++ Infrastructure/Configurations/ProductTypeConfiguration.cs
@@ -0,0 +1,13 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+public class ProductTypeConfiguration : IEntityTypeConfiguration<ProductType>
+{
+  public void Configure(EntityTypeBuilder<ProductType> builder)
+  {
+    builder.Property(p => p.Id).IsRequired();
+    builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+  }
+}
