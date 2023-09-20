f7d211d1dfb123bcbfbc48feacb0783446693704
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-09-20 12:26:48 +0200

    adjust for db change

--- Core/Interfaces/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-using Core.Entities;
-
-namespace Core.Interfaces;
-
-public interface IProductRepository
-{
-  Task<Product> GetProductByIdAync(int id);
-  Task<IReadOnlyList<Product>> GetProductsAync();
-}
