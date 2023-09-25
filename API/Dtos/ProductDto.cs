333c55f5f99509890f250f0db1c660794944d6f8
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-09-25 15:46:47 +0200

    Add quantity to product entity & description text

--- API/Dtos/ProductDto.cs
+++ API/Dtos/ProductDto.cs
@@ -7,6 +7,7 @@ public class ProductDto
   public string Name { get; set; } = "";
   public string Description { get; set; } = "";
   public decimal Price { get; set; }
+  public int Quantity { get; set; }
   public IEnumerable<ImageDto> Images { get; set; } = new List<ImageDto>();
   public string ProductType { get; set; } = "";
 }
--- Infrastructure/Configurations/ProductConfiguration.cs
+++ Infrastructure/Configurations/ProductConfiguration.cs
@@ -9,8 +9,9 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
   {
     builder.Property(p => p.Id).IsRequired();
     builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
-    builder.Property(p => p.Description).IsRequired().HasMaxLength(250);
+    builder.Property(p => p.Description).IsRequired().HasMaxLength(300);
     builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18,2)");
+    builder.Property(p => p.Quantity).IsRequired();
     builder.HasOne(b => b.ProductType).WithMany().HasForeignKey(p => p.ProductTypeId);
     builder.HasMany(p=>p.Images).WithOne(x=>x.Product).HasForeignKey(x=>x.ProductId);
   }
