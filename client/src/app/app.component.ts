c2d4ced6b228c51fcb63537b63cb1a947b6cd0f6
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-10-02 11:40:35 +0200

    connect to API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from './../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Yerba Mate Sotre 2.0';
+export class AppComponent implements OnInit {
+
+  public title: string = 'Yerba Mate Sotre 2.0';
+
+  public constructor(private http: HttpClient) {
+
+  }
+
+  public ngOnInit(): void {   
+    this.http.get(`${environment.apiUrl}/products/`).subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    }
+    )
+  }
 }
