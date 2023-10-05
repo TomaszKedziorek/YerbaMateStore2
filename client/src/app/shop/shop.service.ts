873fafe43a7c18a7d48045086db86e8276dcd3f0
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-10-05 09:54:26 +0200

    Create shop service

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IPagination } from '../shared/models/IPagination';
+import { IProduct } from '../shared/models/IProduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor() { }
+  private baseUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getProducts<TProduct>() {
+    return this.http.get<IPagination<TProduct>>(this.baseUrl + '/products?pageSize=50');
+  }
 }
