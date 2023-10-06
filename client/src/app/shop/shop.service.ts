5f787dc07b2100d513c510935fa4d8c3da5fc976
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-10-06 14:03:43 +0200

    shop.service: get brands, types, countries

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IPagination } from '../shared/models/IPagination';
-import { IProduct } from '../shared/models/IProduct';
+import { IProductBrand } from '../shared/models/IProductBrand';
+import { ICountry } from '../shared/models/ICountry';
+import { IProductType } from '../shared/models/IProductType';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,21 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  public getProducts<TProduct>() {
-    return this.http.get<IPagination<TProduct>>(this.baseUrl + '/products?pageSize=50');
+  public getProducts<TProduct>(productTypeName: string = "") {
+    productTypeName = productTypeName ? '/' + productTypeName : "";
+    return this.http.get<IPagination<TProduct>>(
+      `${this.baseUrl}/products${productTypeName}?pageSize=8`);
+  }
+  
+  public getProductBrands() {
+    return this.http.get<IProductBrand[]>(this.baseUrl + '/products/brands');
+  }
+
+  public getProductTypes() {
+    return this.http.get<IProductType[]>(this.baseUrl + '/products/types');
+  }
+
+  public getProductCountries() {
+    return this.http.get<ICountry[]>(this.baseUrl + '/products/countries');
   }
 }
