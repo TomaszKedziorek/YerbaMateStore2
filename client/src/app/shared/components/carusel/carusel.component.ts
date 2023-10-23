e65877d38a328e28c34c7ac5276db41b50e946b2
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-10-23 16:20:53 +0200

    refactor carusel component

--- client/src/app/shared/components/carusel/carusel.component.ts
+++ client/src/app/shared/components/carusel/carusel.component.ts
@@ -14,10 +14,10 @@ export class CaruselComponent implements OnInit {
   public slideConfig = {
     infinite: true,
     vertical: false,
-    autoplay: false,
-    autoPlaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 5000,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll:3,
     dots: true,
     arrow:true,
     responsive: [
@@ -43,16 +43,8 @@ export class CaruselComponent implements OnInit {
       }
     ]
   };
-  public itemsPerSlide = 3;
-  public singleSlideOffset = true;
 
-  ngOnInit(): void {
-    this.slides.push({ id: 11, pictureUrl: "https://apod.nasa.gov/apod/image/2310/C2023H2LemmonGalaxies.jpg" },)
-    this.slides.push({ id: 12, pictureUrl: "https://apod.nasa.gov/apod/image/2310/AnnularMontagev2.jpg" },)
-    this.slides.push({ id: 14, pictureUrl: "https://apod.nasa.gov/apod/image/2003/wr124_hubbleschmidt_1289.jpg" },)
-    this.slides.push({ id: 15, pictureUrl: "https://apod.nasa.gov/apod/image/2003/Galassiainunagoccia.jpg" },)
-    this.slides.push({ id: 16, pictureUrl: "https://apod.nasa.gov/apod/image/2003/ag-obj-52945-001-pub-large.jpg" },)
-  }
+  ngOnInit(): void {}
 
   onImageClick(pictureUrl: string) {
     document.getElementById('product-detail')?.setAttribute('src', pictureUrl);
