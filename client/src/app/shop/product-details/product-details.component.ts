d022da75bc1855f959a2a1925e7cae9d6ebb465e
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2024-01-24 16:53:24 +0100

    implement specificProductDetails components in Product Detail

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/IProduct';
 import { ShopService } from '../shop.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductType } from 'src/app/shared/models/product-type';
 
 @Component({
   selector: 'app-product-details',
@@ -11,22 +12,39 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
 
   public product!: any;
-  public iproduct!: IProduct;
   public quantity: number = 1;
+  public productType!: ProductType;
+  public allTypes = ProductType;
 
-  constructor(private shopService: ShopService, private route: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     let id = this.route.snapshot.paramMap.get('id');
-    let typeName = this.route.snapshot.paramMap.get('productTypeName') ?? "";
-    this.loadProduct(Number(id), typeName);
+    let productTypeName = this.route.snapshot.paramMap.get('productTypeName') ?? "";
+    this.productType = this.setProductType(productTypeName);
+    this.loadProduct(Number(id), productTypeName);
+  }
+
+  private setProductType(productTypeName: string): number {
+    switch (productTypeName.toUpperCase()) {
+      case ProductType[ProductType.YERBAMATE]:
+        return ProductType.YERBAMATE;
+      case ProductType[ProductType.BOMBILLA]:
+        return ProductType.BOMBILLA;
+      case ProductType[ProductType.CUP]:
+        return ProductType.CUP;
+      default: return 0;
+    }
   }
 
   public loadProduct(id: number, productType: string) {
     this.shopService.getProduct<IProduct>(id, productType).subscribe({
       next: result => {
         if (result) {
-          this.product = result;
+          if (!productType)
+            this.router.navigate(['/shop/', result.productType.toLowerCase().replace(/\s/g, ''), id]);
+          else
+            this.product = result;
         }
       },
       error: err => console.log(err)
@@ -36,6 +54,7 @@ export class ProductDetailsComponent implements OnInit {
   public minusOne() {
     this.quantity = this.quantity - 1 <= 0 ? 1 : this.quantity - 1;
   }
+
   public plusOne() {
     this.quantity = this.quantity >= this.product.quantity ? this.quantity : this.quantity + 1;
   }
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,18 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { PopoverModule } from 'ngx-bootstrap/popover';
 import { YerbaMateSideFilterComponent } from './yerba-mate-side-filter/yerba-mate-side-filter.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from "@angular/router"; 
+import { RouterModule } from "@angular/router";
+import { YerbaMateDetailsComponent } from './specificProductDetails/yerba-mate-details/yerba-mate-details.component';
+import { BombillaDetailsComponent } from './specificProductDetails/bombilla-details/bombilla-details.component';
+import { CupDetailsComponent } from './specificProductDetails/cup-details/cup-details.component'; 
 
 @NgModule({
   declarations: [
     ShopComponent,
     ProductItemComponent,
     YerbaMateSideFilterComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    YerbaMateDetailsComponent,
+    BombillaDetailsComponent,
+    CupDetailsComponent
   ],
   imports: [
     CommonModule,
